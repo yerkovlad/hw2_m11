f92ad3f1b522b83c0b521bbb660f86311a859a60
yerkovlad <61785699+yerkovlad@users.noreply.github.com>
2023-12-10 15:56:06 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,16 +1,35 @@
-from fastapi import FastAPI, HTTPException
-from sqlalchemy import create_engine, Column, Integer, String, DateTime, func
-from sqlalchemy.ext.declarative import DeclarativeMeta, declarative_base
-from sqlalchemy.orm import sessionmaker
-from databases import Database
-from app.models.contact import Contact
+from fastapi import FastAPI, Depends, HTTPException
+from sqlalchemy.orm import Session
+from app.database import database, SessionLocal
+from app.models.contact import Contact, ContactCreate, ContactUpdate, ContactSearch, ContactList, upcoming_birthdays, search_contacts, create_contact, get_contacts, get_contact, update_contact, delete_contact
 
 app = FastAPI()
 
-DATABASE_URL = "postgresql://username:password@localhost/dbname"
-database = Database(DATABASE_URL)
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
-engine = create_engine(DATABASE_URL)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+@app.post("/contacts/", response_model=Contact)
+def create_new_contact(contact: ContactCreate, db: Session = Depends(get_db)):
+    return create_contact(db, contact)
 
-Base: DeclarativeMeta = declarative_base()
+@app.get("/contacts/", response_model=ContactList)
+def read_contacts(skip: int = 0, limit: int = 10, db: Session = Depends(get_db)):
+    return get_contacts(db, skip=skip, limit=limit)
+
+@app.get("/contacts/{contact_id}", response_model=Contact)
+def read_contact(contact_id: int, db: Session = Depends(get_db)):
+    return get_contact(db, contact_id)
+
+@app.put("/contacts/{contact_id}", response_model=Contact)
+def update_existing_contact(contact_id: int, update_data: ContactUpdate, db: Session = Depends(get_db)):
+    return update_contact(db, contact_id, update_data.dict())
+
+@app.delete("/contacts/{contact_id}", response_model=Contact)
+def delete_existing_contact(contact_id: int, db: Session = Depends(get_db)):
+    return delete_contact(db, contact_id)
+
+@app.get("/contacts/search/", res
