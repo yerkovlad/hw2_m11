347e7c261614834b2b06644f4e51b8cbe7826d53
yerkovlad <61785699+yerkovlad@users.noreply.github.com>
2023-12-10 20:57:39 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,35 +1,15 @@
-from fastapi import FastAPI, Depends, HTTPException
-from sqlalchemy.orm import Session
-from app.database import database, SessionLocal
-from app.models.contact import Contact, ContactCreate, ContactUpdate, ContactSearch, ContactList, upcoming_birthdays, search_contacts, create_contact, get_contacts, get_contact, update_contact, delete_contact
+from fastapi import FastAPI
+from src.routes import contacts
+from src.database.db import engine
 
 app = FastAPI()
 
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
+# Include routes
+app.include_router(contacts.router)
 
-@app.post("/contacts/", response_model=Contact)
-def create_new_contact(contact: ContactCreate, db: Session = Depends(get_db)):
-    return create_contact(db, contact)
+# Create the database tables
+def create_tables():
+    from src.database.models import Base
+    Base.metadata.create_all(bind=engine)
 
-@app.get("/contacts/", response_model=ContactList)
-def read_contacts(skip: int = 0, limit: int = 10, db: Session = Depends(get_db)):
-    return get_contacts(db, skip=skip, limit=limit)
-
-@app.get("/contacts/{contact_id}", response_model=Contact)
-def read_contact(contact_id: int, db: Session = Depends(get_db)):
-    return get_contact(db, contact_id)
-
-@app.put("/contacts/{contact_id}", response_model=Contact)
-def update_existing_contact(contact_id: int, update_data: ContactUpdate, db: Session = Depends(get_db)):
-    return update_contact(db, contact_id, update_data.dict())
-
-@app.delete("/contacts/{contact_id}", response_model=Contact)
-def delete_existing_contact(contact_id: int, db: Session = Depends(get_db)):
-    return delete_contact(db, contact_id)
-
-@app.get("/contacts/search/", res
+create_tables()
