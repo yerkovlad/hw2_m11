ad24eab9a6933ed68e0cf15257b4d8c94edd349a
yerkovlad <61785699+yerkovlad@users.noreply.github.com>
2023-12-10 20:53:43 +0200

    Update db.py

--- app/src/database/db.py
+++ app/src/database/db.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import DeclarativeMeta, declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://yerkovlad:02012009@localhost/yerkov"
+DATABASE_URL = "postgresql://yerkovlad:02012009@localhost/yerv"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
