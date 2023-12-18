0ab5526fe1fac8b726f54813f3611a9132718636
yerkovlad <61785699+yerkovlad@users.noreply.github.com>
2023-12-18 23:35:11 +0200

    Update contacts.py

--- app/src/routes/contacts.py
+++ app/src/routes/contacts.py
@@ -1,4 +1,3 @@
-# src/routes/contacts.py
 from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.orm import Session
 from src.database.db import db
@@ -35,3 +34,23 @@ def login_for_access_token(contact: ContactCreate, db: Session = Depends(db.get_
 @router.get("/users/me", response_model=ContactList)
 def read_user_me(current_user: Contact = Depends(get_current_user), db: Session = Depends(db.get_db)):
     return contacts.get_contact(db, current_user.id)
+
+@router.post("/contacts/", response_model=ContactList)
+def create_new_contact(contact: ContactCreate, db: Session = Depends(db.get_db), current_user: Contact = Depends(get_current_user)):
+    return contacts.create_contact(db, contact, current_user.id)
+
+@router.get("/contacts/", response_model=ContactList)
+def read_contacts(skip: int = 0, limit: int = 10, db: Session = Depends(db.get_db), current_user: Contact = Depends(get_current_user)):
+    return contacts.get_contacts(db, skip=skip, limit=limit, user_id=current_user.id)
+
+@router.get("/contacts/{contact_id}", response_model=ContactList)
+def read_contact(contact_id: int, db: Session = Depends(db.get_db), current_user: Contact = Depends(get_current_user)):
+    return contacts.get_contact(db, contact_id, user_id=current_user.id)
+
+@router.put("/contacts/{contact_id}", response_model=ContactList)
+def update_existing_contact(contact_id: int, update_data: ContactUpdate, db: Session = Depends(db.get_db), current_user: Contact = Depends(get_current_user)):
+    return contacts.update_contact(db, contact_id, update_data.dict(), user_id=current_user.id)
+
+@router.delete("/contacts/{contact_id}", response_model=ContactList)
+def delete_existing_contact(contact_id: int, db: Session = Depends(db.get_db), current_user: Contact = Depends(get_current_user)):
+    return contacts.delete_contact(db, contact_id, user_id=current_user.id)
