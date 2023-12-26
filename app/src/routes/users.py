c2d525251138811e01ba34daf622360edec8df6f
yerkovlad <61785699+yerkovlad@users.noreply.github.com>
2023-12-26 11:36:33 +0200

    Update users.py

--- app/src/routes/users.py
+++ app/src/routes/users.py
@@ -1,33 +1,37 @@
-from fastapi import APIRouter, Depends, HTTPException
-from fastapi_mail import MessageSchema
+from fastapi import APIRouter, Depends, HTTPException, UploadFile, File
 from sqlalchemy.orm import Session
-from app import schemas, crud, models, utils, database
+from app import crud, schemas, database, utils
+from fastapi_limiter import FastAPILimiter
+from fastapi_limiter.depends import RateLimiter
+from cloudinary.uploader import upload
+from cloudinary.utils import cloudinary_url
 
 router = APIRouter()
 
-@router.post("/register", response_model=MessageSchema)
-async def register_user(user: schemas.UserCreate, db: Session = Depends(database.get_db)):
-    db_user = crud.get_user_by_email(db, email=user.email)
-    if db_user:
-        raise HTTPException(status_code=400, detail="Email already registered")
-    hashed_password = utils.get_password_hash(user.password)
-    db_user = crud.create_user(db, user, hashed_password)
-    
-    await utils.send_email_verification(db_user.email)
-    
-    return {"message": "Registration successful. Check your email for verification."}
+limiter = RateLimiter(
+    key_func=utils.get_current_user_email,
+    rate="1/second",
+    block=True,
+)
 
-@router.get("/verify/{token}", response_model=MessageSchema)
-async def verify_email(token: str, db: Session = Depends(database.get_db)):
-    email = await utils.verify_email_token(token)
+FastAPILimiter.init(limiter)
 
-    db_user = crud.get_user_by_email(db, email=email)
+@router.post("/avatar", response_model=schemas.User)
+async def update_avatar(
+    file: UploadFile = File(...),
+    current_user: schemas.User = Depends(utils.get_current_user),
+    db: Session = Depends(database.get_db)
+):
+    await FastAPILimiter.check_key(
+        limiter,
+        utils.get_current_user_email(current_user),
+        request=request
+    )
+    cloudinary_response = upload(file.file)
+    avatar_url = cloudinary_url(cloudinary_response['public_id'], format=cloudinary_response['format'])[0]
+    db_user = crud.get_user_by_email(db, email=current_user.email)
     if db_user:
-        crud.update_user_verification_status(db, user=db_user, is_verified=True)
-        return {"message": "Email verification successful."}
+        crud.update_user_avatar_url(db, user=db_user, avatar_url=avatar_url)
+        return db_user
     else:
         raise HTTPException(status_code=404, detail="User not found")
-
-@router.get("/profile", response_model=schemas.User)
-async def get_user_profile(current_user: schemas.User = Depends(utils.get_current_user)):
-    return current_user
