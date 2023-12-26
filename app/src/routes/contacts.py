b8902c4baee92950651abebbc9e3286912c03963
yerkovlad <61785699+yerkovlad@users.noreply.github.com>
2023-12-26 11:34:01 +0200

    Update contacts.py

--- app/src/routes/contacts.py
+++ app/src/routes/contacts.py
@@ -1,56 +1,29 @@
-from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
-from src.database.db import db
-from src.repository import contacts
-from src.schemas.contact import ContactCreate, ContactUpdate, ContactList
-from src.schemas.token import Token, create_access_token, get_current_user
+from app import crud, schemas, database, utils
+from fastapi_limiter import FastAPILimiter
+from fastapi_limiter.depends import RateLimiter
 
 router = APIRouter()
 
-@router.post("/register/", response_model=Token)
-def register_user(contact: ContactCreate, db: Session = Depends(db.get_db)):
-    existing_user = db.query(Contact).filter(Contact.email == contact.email).first()
-    if existing_user:
-        raise HTTPException(
-            status_code=status.HTTP_409_CONFLICT,
-            detail="User with this email already exists",
-        )
-    user = contacts.register_user(db, contact)
-    access_token = create_access_token(data=create_access_token_data(user))
-    return {"access_token": access_token, "token_type": "bearer"}
+limiter = RateLimiter(
+    key_func=utils.get_current_user_email,
+    rate="1/second",
+    block=True,
+)
 
-@router.post("/token/", response_model=Token)
-def login_for_access_token(contact: ContactCreate, db: Session = Depends(db.get_db)):
-    user = contacts.authenticate_user(db, contact.email, contact.password)
-    if not user:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Incorrect email or password",
-            headers={"WWW-Authenticate": "Bearer"},
-        )
-    access_token = create_access_token(data=create_access_token_data(user))
-    return {"access_token": access_token, "token_type": "bearer"}
+FastAPILimiter.init(limiter)
 
-@router.get("/users/me", response_model=ContactList)
-def read_user_me(current_user: Contact = Depends(get_current_user), db: Session = Depends(db.get_db)):
-    return contacts.get_contact(db, current_user.id)
-
-@router.post("/contacts/", response_model=ContactList)
-def create_new_contact(contact: ContactCreate, db: Session = Depends(db.get_db), current_user: Contact = Depends(get_current_user)):
-    return contacts.create_contact(db, contact, current_user.id)
-
-@router.get("/contacts/", response_model=ContactList)
-def read_contacts(skip: int = 0, limit: int = 10, db: Session = Depends(db.get_db), current_user: Contact = Depends(get_current_user)):
-    return contacts.get_contacts(db, skip=skip, limit=limit, user_id=current_user.id)
-
-@router.get("/contacts/{contact_id}", response_model=ContactList)
-def read_contact(contact_id: int, db: Session = Depends(db.get_db), current_user: Contact = Depends(get_current_user)):
-    return contacts.get_contact(db, contact_id, user_id=current_user.id)
-
-@router.put("/contacts/{contact_id}", response_model=ContactList)
-def update_existing_contact(contact_id: int, update_data: ContactUpdate, db: Session = Depends(db.get_db), current_user: Contact = Depends(get_current_user)):
-    return contacts.update_contact(db, contact_id, update_data.dict(), user_id=current_user.id)
-
-@router.delete("/contacts/{contact_id}", response_model=ContactList)
-def delete_existing_contact(contact_id: int, db: Session = Depends(db.get_db), current_user: Contact = Depends(get_current_user)):
-    return contacts.delete_contact(db, contact_id, user_id=current_user.id)
+@router.post("/contacts/", response_model=schemas.Contact)
+async def create_contact(
+    contact: schemas.ContactCreate,
+    current_user: schemas.User = Depends(utils.get_current_user),
+    db: Session = Depends(database.get_db)
+):
+    await FastAPILimiter.check_key(
+        limiter, 
+        utils.get_current_user_email(current_user), 
+        request=request
+    )
+    
+    return crud.create_contact(db, contact, current_user)
