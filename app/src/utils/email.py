24f5f4f2a63eefe4c50789ef187e186487720850
yerkovlad <61785699+yerkovlad@users.noreply.github.com>
2023-12-26 11:30:28 +0200

    Update email.py

--- app/src/utils/email.py
+++ app/src/utils/email.py
@@ -1,29 +1,39 @@
 from fastapi_mail import FastMail, MessageSchema, ConnectionConfig
 from typing import List, Union
 from fastapi import HTTPException
+from app.utils.token import create_access_token, decode_token
 
 conf = ConnectionConfig(
-    MAIL_USERNAME="email@yourdomain.com",
-    MAIL_PASSWORD="email_password",
-    MAIL_FROM="email@yourdomain.com",
+    MAIL_USERNAME="mail_username@yourdomain.com",
+    MAIL_PASSWORD="mail_password",
+    MAIL_FROM="mail_username@yourdomain.com",
     MAIL_PORT=465,
-    MAIL_SERVER="",
+    MAIL_SERVER="smtp.mail.com",
     MAIL_TLS=False,
     MAIL_SSL=True,
     USE_CREDENTIALS=True,
     VALIDATE_CERTS=True
 )
 
-async def send_email(to_email: Union[str, List[str]], subject: str, message: str):
-    message = MessageSchema(
-        subject=subject,
-        recipients=[to_email] if isinstance(to_email, str) else to_email,
-        body=message,
-        subtype="html"
-    )
-
+async def send_email_verification(to_email: str):
+    subject = "Email Verification"
+    message = f"Click the link below to verify your email:\n\nhttp://your-api-domain.com/verify?token={create_access_token({'sub': to_email})}"
+    
     try:
+        message = MessageSchema(
+            subject=subject,
+            recipients=[to_email],
+            body=message,
+            subtype="html"
+        )
         fm = FastMail(conf)
         await fm.send_message(message)
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Failed to send email: {str(e)}")
+
+async def verify_email_token(token: str):
+    try:
+        payload = decode_token(token)
+        return payload["sub"]
+    except JWTError:
+        raise HTTPException(status_code=401, detail="Invalid email verification token")
