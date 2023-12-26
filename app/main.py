8b2301ae31776266f4c4854867237bcfa87d973d
yerkovlad <61785699+yerkovlad@users.noreply.github.com>
2023-12-26 11:38:58 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -2,8 +2,18 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from src.routes import contacts
 from src.database.db import engine
+from dotenv import load_dotenv
+import os
 
 app = FastAPI()
+
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+SECRET_KEY = os.getenv("SECRET_KEY")
+CLOUDINARY_CLOUD_NAME = os.getenv("CLOUDINARY_CLOUD_NAME")
+CLOUDINARY_API_KEY = os.getenv("CLOUDINARY_API_KEY")
+CLOUDINARY_API_SECRET = os.getenv("CLOUDINARY_API_SECRET")
 
 origins = [
     "http://localhost:3000"
