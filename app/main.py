f8822ddc5d8f2ac02751b034052dcc8f0e1d1831
yerkovlad <61785699+yerkovlad@users.noreply.github.com>
2023-12-26 11:45:28 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -14,6 +14,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 CLOUDINARY_CLOUD_NAME = os.getenv("CLOUDINARY_CLOUD_NAME")
 CLOUDINARY_API_KEY = os.getenv("CLOUDINARY_API_KEY")
 CLOUDINARY_API_SECRET = os.getenv("CLOUDINARY_API_SECRET")
+ALGORITHM = os.getenv("")
 
 origins = [
     "http://localhost:3000"
