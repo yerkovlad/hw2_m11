a65bd30ac15a3f4acfbb2b681601c965a2a3e443
yerkovlad <61785699+yerkovlad@users.noreply.github.com>
2023-12-26 11:35:42 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,13 +1,24 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from src.routes import contacts
 from src.database.db import engine
 
 app = FastAPI()
 
-# Include routes
+origins = [
+    "http://localhost:3000"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(contacts.router)
 
-# Create the database tables
 def create_tables():
     from src.database.models import Base
     Base.metadata.create_all(bind=engine)
