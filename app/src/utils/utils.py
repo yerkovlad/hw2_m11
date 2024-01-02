96c78a31b2cc6b1dba79a104774730522c6e4c9f
yerkovlad <61785699+yerkovlad@users.noreply.github.com>
2024-01-02 14:21:21 +0200

    Update utils.py

--- app/src/utils/utils.py
+++ app/src/utils/utils.py
@@ -5,12 +5,32 @@ from passlib.context import CryptContext
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 def verify_password(plain_password, hashed_password):
+    """
+    Verify a password against a hashed version.
+
+    :param plain_password: The plain text password.
+    :param hashed_password: The hashed password.
+    :return: Boolean indicating if the password is correct.
+    """
     return pwd_context.verify(plain_password, hashed_password)
 
 def get_password_hash(password):
+    """
+    Hash a password.
+
+    :param password: The plain text password to hash.
+    :return: The hashed password.
+    """
     return pwd_context.hash(password)
 
 def create_access_token(data: dict, expires_delta: timedelta | None = None):
+    """
+    Create an access token.
+
+    :param data: The payload data to be encoded in the token.
+    :param expires_delta: Optional expiration time delta for the token.
+    :return: The encoded JWT access token.
+    """
     to_encode = data.copy()
     if expires_delta:
         expire = datetime.utcnow() + expires_delta
@@ -21,6 +41,12 @@ def create_access_token(data: dict, expires_delta: timedelta | None = None):
     return encoded_jwt
 
 def decode_access_token(token: str):
+    """
+    Decode an access token.
+
+    :param token: The JWT token to be decoded.
+    :return: The decoded payload of the token or None if token is invalid.
+    """
     try:
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
         return payload
@@ -28,11 +54,23 @@ def decode_access_token(token: str):
         return None
 
 def generate_reset_token(email: str):
+    """
+    Generate a password reset token.
+
+    :param email: Email for which the reset token is being generated.
+    :return: JWT token for password reset.
+    """
     expiration = datetime.utcnow() + timedelta(minutes=RESET_TOKEN_EXPIRE_MINUTES)
     data = {"sub": email, "exp": expiration}
     return jwt.encode(data, SECRET_KEY, algorithm=ALGORITHM)
 
 def verify_reset_token(token: str):
+    """
+    Verify a password reset token.
+
+    :param token: The JWT token to be verified.
+    :return: The email associated with the token if valid, None otherwise.
+    """
     try:
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
         email: str = payload.get("sub")
