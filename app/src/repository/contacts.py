8a3e5f128f2e66964e986c663a78cf83e75fa4e5
yerkovlad <61785699+yerkovlad@users.noreply.github.com>
2024-01-02 14:11:19 +0200

    Update contacts.py

--- app/src/repository/contacts.py
+++ app/src/repository/contacts.py
@@ -5,6 +5,13 @@ from src.schemas.token import create_access_token
 from src.database.db import password_hasher
 
 def register_user(db: Session, contact: ContactCreate):
+    """
+    Register a new user in the database.
+
+    :param db: The database session.
+    :param contact: The contact information for the new user.
+    :return: The created user object.
+    """
     db_contact = Contact(**contact.dict(), hashed_password=password_hasher.create_password_hash(contact.password))
     db.add(db_contact)
     db.commit()
@@ -12,10 +19,24 @@ def register_user(db: Session, contact: ContactCreate):
     return db_contact
 
 def authenticate_user(db: Session, email: str, password: str):
+    """
+    Authenticate a user based on email and password.
+
+    :param db: The database session.
+    :param email: The email of the user.
+    :param password: The password of the user.
+    :return: The authenticated user or None if authentication fails.
+    """
     user = db.query(Contact).filter(Contact.email == email).first()
     if user and password_hasher.verify_password(password, user.hashed_password):
         return user
     return None
 
 def create_access_token_data(user: Contact):
+    """
+    Create access token data for a user.
+
+    :param user: The user object for which the access token is created.
+    :return: A dictionary containing the access token data.
+    """
     return {"sub": user.email}
