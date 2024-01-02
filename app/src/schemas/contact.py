274098283fb30dd51a83ab1c588821424ff0905d
yerkovlad <61785699+yerkovlad@users.noreply.github.com>
2024-01-02 14:14:42 +0200

    Update contact.py

--- app/src/schemas/contact.py
+++ app/src/schemas/contact.py
@@ -3,6 +3,16 @@ from pydantic import BaseModel
 from datetime import datetime
 
 class ContactBase(BaseModel):
+    """
+    Base Pydantic model for contact data.
+
+    :param first_name: The first name of the contact.
+    :param last_name: The last name of the contact.
+    :param email: The email of the contact.
+    :param phone_number: The phone number of the contact.
+    :param birthday: The birthday of the contact.
+    :param additional_data: Additional data for the contact (optional).
+    """
     first_name: str
     last_name: str
     email: str
@@ -11,19 +21,46 @@ class ContactBase(BaseModel):
     additional_data: str = None
 
 class ContactCreate(ContactBase):
+    """
+    Pydantic model for creating a new contact.
+
+    Inherits from ContactBase.
+    """
     pass
 
 class ContactUpdate(ContactBase):
+    """
+    Pydantic model for updating an existing contact.
+
+    Inherits from ContactBase.
+    """
     pass
 
 class Contact(ContactBase):
+    """
+    Pydantic model for displaying contact details.
+
+    Inherits from ContactBase and includes an additional 'id' field.
+
+    :param id: The unique identifier for the contact.
+    """
     id: int
 
     class Config:
         orm_mode = True
 
 class ContactSearch(ContactBase):
+    """
+    Pydantic model for searching contacts.
+
+    Inherits from ContactBase.
+    """
     pass
 
 class ContactList(BaseModel):
+    """
+    Pydantic model for displaying a list of contacts.
+
+    :param contacts: List of Contact models.
+    """
     contacts: List[Contact]
